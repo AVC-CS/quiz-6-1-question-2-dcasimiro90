10b4a4e1aab9ab8c01bdb3792f9f021b851211e7
dcasimiro90 <casimiro444@gmail.com>
2024-10-29 02:18:06 -0700

    done

--- main.cpp
+++ main.cpp
@@ -1,35 +1,21 @@
 #include "main.hpp"
-#include <fstream>
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-// Function to write employee data to a file
-int writeFile(const vector<Employee>& employees, const string& filename) {
-    ofstream outFile(filename);
-    if (!outFile) {
-        cerr << "Error opening file for writing." << endl;
-        return -1;
+int main() {
+    string filename = "employee.txt";
+
+    // Writing to file
+    int empNum = writeFile(filename);
+    if (empNum > 0) {
+        cout << "File created with " << empNum << " employee records.\n";
     }
 
-    for (const auto& emp : employees) {
-        outFile << emp.id << " " << emp.name << " " << emp.department << " " << emp.salary << endl;
-    }
-    outFile.close();
-    return employees.size(); // Return the number of employees written
-}
+    // Reading from file
+    empNum = readFile(filename);
+    cout << "The total number of employees read: " << empNum << endl;
 
-// Function to read employee data from a file
-int readFile(const string& filename, vector<Employee>& employees) {
-    ifstream inFile(filename);
-    if (!inFile) {
-        cerr << "Error opening file for reading." << endl;
-        return -1;
-    }
-
-    Employee emp;
-    while (inFile >> emp.id >> emp.name >> emp.department >> emp.salary) {
-        employees.push_back(emp);
-    }
-    inFile.close();
-    return employees.size(); // Return the number of employees read
+    return 0;
 }
--- main.hpp
+++ main.hpp
@@ -1,114 +1,76 @@
-/***************************************************
- * Code your program here
- ***************************************************/
-
-
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <iomanip>
-#include <vector>
 
 using namespace std;
 
-struct Employee {
-    int id;
-    string name;
-    string department;
-    double salary;
-};
-
-// Function to get employee details from the user
-void getEmployeeData(vector<Employee>& employees, int numEmployees) {
-    for (int i = 0; i < numEmployees; ++i) {
-        Employee emp;
-        cout << "Enter details for employee " << (i + 1) << endl;
-        cout << "ID: ";
-        cin >> emp.id;
-        cout << "Name: ";
-        cin >> emp.name;
-        cout << "Department: ";
-        cin >> emp.department;
-        cout << "Salary: ";
-        cin >> emp.salary;
-        employees.push_back(emp);
-    }
-}
-
-// Function to write employee data to a file
-void writeToFile(const vector<Employee>& employees, const string& filename) {
-    ofstream outFile(filename);
-    if (outFile.is_open()) {
-        for (const auto& emp : employees) {
-            outFile << emp.id << " " << emp.name << " " << emp.department << " " << emp.salary << endl;
-        }
-        outFile.close();
-        cout << "Data successfully written to " << filename << endl;
-    } else {
-        cout << "Error opening file for writing." << endl;
-    }
-}
-
-// Function to read employee data from a file
-vector<Employee> readFromFile(const string& filename) {
-    vector<Employee> employees;
-    ifstream inFile(filename);
-    if (inFile.is_open()) {
-        Employee emp;
-        while (inFile >> emp.id >> emp.name >> emp.department >> emp.salary) {
-            employees.push_back(emp);
-        }
-        inFile.close();
-    } else {
-        cout << "Error opening file for reading." << endl;
-    }
-    return employees;
-}
-
-// Function to display employee data and calculate total/average salary
-void displayEmployeeData(const vector<Employee>& employees) {
-    double totalSalary = 0;
-    int numEmployees = employees.size();
-
-    cout << left << setw(10) << "ID" 
-         << setw(15) << "Name" 
-         << setw(15) << "Department" 
-         << setw(10) << "Salary" << endl;
-    cout << "-------------------------------------------------" << endl;
-
-    for (const auto& emp : employees) {
-        cout << left << setw(10) << emp.id 
-             << setw(15) << emp.name 
-             << setw(15) << emp.department 
-             << setw(10) << emp.salary << endl;
-        totalSalary += emp.salary;
+int writeFile(const string &filename) {
+    ofstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Error opening file!" << endl;
+        return -1;
     }
 
-    double averageSalary = numEmployees > 0 ? totalSalary / numEmployees : 0;
-
-    cout << "\nTotal Salary: " << totalSalary << endl;
-    cout << "Average Salary: " << averageSalary << endl;
-}
-
-int main() {
-    string filename = "employee.txt";
-    int numEmployees;
-    vector<Employee> employees;
-
+    int empNum;
     cout << "Enter the number of employees: ";
-    cin >> numEmployees;
+    cin >> empNum;
 
-    // Get employee data from user
-    getEmployeeData(employees, numEmployees);
+    for (int i = 0; i < empNum; i++) {
+        int id;
+        string name, department;
+        double salary;
 
-    // Write data to file
-    writeToFile(employees, filename);
+        cout << "Enter details for employee " << (i + 1) << ":\n";
+        cout << "ID: ";
+        cin >> id;
+        cout << "Name: ";
+        cin.ignore();
+        getline(cin, name);
+        cout << "Department: ";
+        getline(cin, department);
+        cout << "Salary: ";
+        cin >> salary;
 
-    // Read data from file
-    employees = readFromFile(filename);
+        // Write employee details to the file
+        file << id << " " << name << " " << department << " " << salary << endl;
+    }
 
-    // Display data and calculate total/average salary
-    displayEmployeeData(employees);
+    file.close();
+    return empNum;
+}
 
-    return 0;
+int readFile(const string &filename) {
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Error opening file!" << endl;
+        return -1;
+    }
+
+    int id;
+    string name, department;
+    double salary;
+    int empCount = 0;
+    double totalSalary = 0.0;
+
+    cout << "Employee Records:\n";
+    cout << "ID\tName\t\tDepartment\tSalary\n";
+    cout << "------------------------------------------\n";
+
+    while (file >> id >> name >> department >> salary) {
+        cout << id << "\t" << name << "\t\t" << department << "\t\t" << salary << endl;
+        totalSalary += salary;
+        empCount++;
+    }
+
+    file.close();
+
+    if (empCount > 0) {
+        double averageSalary = totalSalary / empCount;
+        cout << "\nTotal Salary: " << totalSalary << "\n";
+        cout << "Average Salary: " << averageSalary << "\n";
+    } else {
+        cout << "No employee records found.\n";
+    }
+
+    return empCount;
 }
